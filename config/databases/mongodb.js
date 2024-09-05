fe7668340ccd701e7baaf43bd4c22411056af68d
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2024-09-05 23:09:35 +0100

    changed the DB to prod.

--- config/databases/mongodb.js
+++ config/databases/mongodb.js
@@ -1,17 +1,15 @@
-const  mongoose = require("mongoose");
-
+const mongoose = require("mongoose");
 
 const connectToDB = async () => {
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect(process.env.MONGODB_URI_DEVELOPMENT);
+    await mongoose.connect(process.env.MONGODB_URI_PRODUCTION);
     console.log("Database connection established successfully!!!");
   } catch (error) {
-      console.log(
-        `There was an error trying to connect to DB!!! ${error.message}`
-      );
+    console.log(
+      `There was an error trying to connect to DB!!! ${error.message}`
+    );
   }
 };
 
-module.exports = {connectToDB}
-
+module.exports = { connectToDB };
